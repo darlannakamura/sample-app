0af4ffbd3b3dcd6f98e0107426ddb18f7f69f6f6
= <darlannakamura@hotmail.com>
2019-02-23 20:05:01 -0300

    removido console.logs

--- test/api.test.js
+++ test/api.test.js
@@ -8,12 +8,6 @@ chai.use(chaiHttp);
 const expect = chai.expect;
 
 describe('Check API status', function(done) {
-    console.log('NODE_ENV');
-    console.log(process.env.NODE_ENV);
-
-    console.log('ENVIRONMENT');
-    console.log(ENVIRONMENT);
-
     it('Should verify API status', function(done) {
         chai.request(server)
             .get('/')
